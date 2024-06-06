c4971960f30db1a9ba62e64d663499ffaaff8174
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2024-06-07 00:20:00 +0100

    feat: express server and socket.io server configuration, App folder as removed

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
-import { createServer } from "http";
+import express from 'express'
 import { Server } from "socket.io";
+// provides utilities for working with file and directory paths
+import path from 'path'
+// converts file URL to path
+import { fileURLToPath } from 'url';
 
-// Creates a server instance
-const httpServer = createServer();
+// full path of the current file
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// create port and configuring the express srver
+const PORT = process.env.PORT || 3500
+const app = express()
+app.use(express.static(path.join(__dirname, "public"))) // serves static fikes from the public directory
+
+// starts the server
+const expressServer = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
+
 // Configuring Socket.io
-const io = new Server(httpServer, {
+const io = new Server(expressServer, {
   cors: {
     origin:
       process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"],
@@ -20,5 +36,3 @@ io.on("connection", (socket) => {
   });
 });
 
-// starting the http Server
-httpServer.listen(3500, () => console.log('listening on port 3500'))
