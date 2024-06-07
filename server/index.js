f21867e7f69b92e3dfdfc55ec1179f92c957103d
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2024-06-07 14:02:16 +0100

    feat: chat activity detection added

--- server/index.js
+++ server/index.js
@@ -1,38 +1,49 @@
-import express from 'express'
+import express from "express";
 import { Server } from "socket.io";
-// provides utilities for working with file and directory paths
-import path from 'path'
-// converts file URL to path
-import { fileURLToPath } from 'url';
-
-// full path of the current file
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-// create port and configuring the express srver
-const PORT = process.env.PORT || 3500
-const app = express()
-app.use(express.static(path.join(__dirname, "public"))) // serves static fikes from the public directory
-
+import path from "path"; //utilities for working with file and directory paths
+import { fileURLToPath } from "url";   // converts file URL to path
+// Handling file paths
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// setting up the express server and servers static files from public directory
+const PORT = process.env.PORT || 3500;
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 // starts the server
 const expressServer = app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
-})
-
+  console.log(`Listening on port ${PORT}`);
+});
 // Configuring Socket.io
 const io = new Server(expressServer, {
+  // Why is comfiguration important?
   cors: {
     origin:
-      process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"],
+      process.env.NODE_ENV === "production"
+        ? false
+        : ["http://localhost:5500", "http://127.0.0.1:5500"],
   },
 });
 // Handling Soket connections
+// on Connetion
 io.on("connection", (socket) => {
   console.log(`User ${socket.id} connected`);
-// Handling Message from clients
+  // Upon connection - only to user
+  socket.emit("message", "Welcome to Chat App!");
+  // Upon connection - to all others
+  socket.broadcast.emit("message", `User ${socket.id.substring(0, 5)} connected`)
+  // Listening for a message event - on message
   socket.on("message", (data) => {
     console.log(data);
-    io.emit('message',`${socket.id.substring(0, 5)}  ${data}`);
+    io.emit("message", `${socket.id.substring(0, 5)}:  ${data}`); // emits message to all connected users
   });
+  // when user disconnets - to all others
+  socket.on("disconnect", ()=> {
+    socket.broadcast.emit("message", `User ${socket.id.substring(0, 5)} disconnected`)
+  })
+  // Listen for activity
+  socket.on('activity', (name) => {
+    socket.broadcast.emit('activity', name)
+  })
 });
+
 
--- server/public/app.js
+++ server/public/app.js
@@ -1,22 +1,40 @@
 // Establish a websocket connection to the server running on port 3500 using socket.io
-const socket = io('ws://localhost:3500')
+const socket = io("ws://localhost:3500");
+// selecting dom elements
+const activity = document.querySelector(".activity");
+const msgInput = document.querySelector("input");
 // Sending Messages
 function sendMessage(e) {
-  e.preventDefault()
-  const input = document.querySelector('input')
-  if (input.value) {
-    socket.emit('message', input.value)
-    input.value = ""
+  e.preventDefault();
+  if (msgInput.value) {
+    socket.emit("message", msgInput.value); // emits message to the server
+    msgInput.value = "";
   }
-  input.focus()
+  msgInput.focus();
 }
-
-document.querySelector('form')
-  .addEventListener('submit', sendMessage)
-
-// Listen for messages
+document.querySelector("form").addEventListener("submit", sendMessage);
+// Receiving messages
 socket.on("message", (data) => {
-  const li = document.createElement('li')
-  li.textContent = data
-  document.querySelector('ul').appendChild(li)
-})
+  // Listens for message events from the server
+  activity.textContent = "";
+  const li = document.createElement("li");
+  li.textContent = data;
+  document.querySelector("ul").appendChild(li); // append each received message to a list(ul) in the DOM
+});
+
+// Typing Activity Indicator
+msgInput.addEventListener('keypress', () => {
+  socket.emit('activity', socket.id.substring(0, 5));
+});
+let activityTimer
+socket.on("activity", (name) => {
+  activity.textContent = `${name} is typing...`;
+
+  // clear after 3 seconds
+  clearTimeout(activityTimer)
+  activityTimer = setTimeout(() => {
+    activity.textContent = ""
+  }, 3000)
+});
+
+
